4e0c4931984b5c142ce2d9526a0cf20d2e3c1d1c
muhammad45401 <saadjani3333@gmail.com>
2024-10-18 18:47:06 +0500

    add text edtior

--- card/Card.jsx
+++ card/Card.jsx
@@ -21,7 +21,9 @@ function Card(props){
        {props.img}
       
       </div>
+      
       </>
+      
     )
     }
     
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from './App.jsx';
 import './index.css';
 import Calculator from './calcultaor/Calcultor.jsx';
 import Like from './likeButton/Like.jsx';
-
+import TextEditor from './textEditor/TextEditor.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App /> 
    <Calculator  />
    <Like/>
+<TextEditor/>
   </StrictMode>
 )
